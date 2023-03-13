80920650e6260f3c73d898bf89d492cbf529658d
alper <alper@ALPEREN-PC>
2023-03-13 19:02:09 +0300

    Add project files.

---
+++ WebAppTask.Api/Startup.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebAppTask.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            int workerThread, threads;
+            ThreadPool.GetAvailableThreads(out workerThread, out threads);
+
+            // Environment.ProcessorCount = 8 for this PC
+            var success = ThreadPool.SetMaxThreads(Environment.ProcessorCount, Environment.ProcessorCount);
+            ThreadPool.GetAvailableThreads(out workerThread, out threads);
+        }
+
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
